ed5c9b9c3bf7b0773e5398cef72de3309925fb9e
Carolina Gonzalez <carolina.gonzalez@mercadolivre.com>
2022-07-21 22:39:18 -0300

    feat: added how long it takes header

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+      <body>
+        <div className="header">How long does it takes</div>
+      </body>
+
+  );
+}
+
+export default App;
